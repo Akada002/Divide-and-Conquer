a5e9f45649099644295351500c3ddf96c2bb8ccd
Akada002 <78979382+Akada002@users.noreply.github.com>
2022-01-18 13:53:41 -0500

    Update Merge.py

--- Merge.py
+++ Merge.py
@@ -9,11 +9,28 @@ def Merge_Sort(A, B):
     if A[i] <= B[j]:
       C.append(A[i])
       i += 1
-
     else:
       C.append(B[j])
       j += 1
   return C
-
-
 print(Merge_Sort(A, B))
+
+________________________________________________
+
+def merge_sort(a,b):
+  sorted_list=[]
+  i=j=0
+
+  while i<len(a) and j<len(b):
+    if a[i]>= b[j]:
+      sorted_list.append(a[i])
+      i+=1
+    else:
+      sorted_list.append(b[j])
+  return sorted_list
+
+if __name__ == '__main__':
+  a=[5,8,12,56]
+  b=[7,9,45,51]
+
+  print(merge_sort(a,b))
